205a5d12d13e740b50a8b63eb97fe1a1e1d2c2e1
vsantsal <vsantsal@gmail.com>
2023-01-08 14:59:36 -0300

    primeira implementacao de find_pacman

--- pacman.py
+++ pacman.py
@@ -13,3 +13,17 @@ game_map = [
     "|........|",
     "|--------|",
 ]
+
+
+def find_pacman(pacman_map):
+    """"""
+    pacman_x = -1
+    pacman_y = -1
+
+    for x in range(len(pacman_map)):
+        for y in range(len(pacman_map[x])):
+            if pacman_map[x][y] == '@':
+                pacman_x = x
+                pacman_y = y
+
+    return pacman_x, pacman_y
