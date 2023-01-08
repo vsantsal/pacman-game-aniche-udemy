98c39a2e9b84944682d62771bfbade26ea9206ff
vsantsal <vsantsal@gmail.com>
2023-01-08 14:42:00 -0300

    modelagem basica do jogo

---
+++ pacman.py
@@ -0,0 +1,15 @@
+# model
+# @ -> pacman (our hero)
+# G -> Ghost
+# P -> Pills
+# . -> empty spaces
+# | and - -> walls
+
+game_map = [
+    "|--------|",
+    "|G..|..G.|",
+    "|...PP...|",
+    "|G....@|.|",
+    "|........|",
+    "|--------|",
+]
