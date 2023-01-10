9770fc1ffdbee4606b93a1f2c1f40a409bf6275d
vsantsal <vsantsal@gmail.com>
2023-01-10 16:53:46 -0300

    modificando programa para movimento de ghosts

--- program.py
+++ program.py
@@ -1,4 +1,4 @@
-from pacman import play
+from pacman import play, move_ghosts
 from ui import ui_print, ui_key, ui_game_finished
 
 mapa = [
@@ -16,6 +16,7 @@ while not game_finished:
     ui_print(mapa)
     key = ui_key()
     valid_key, pacman_is_alive, pacman_won_the_game = play(mapa, key)
-    game_finished = pacman_won_the_game or not pacman_is_alive
+    pacman_was_hit = move_ghosts(mapa)
+    game_finished = pacman_won_the_game or pacman_was_hit or not pacman_is_alive
     if game_finished:
         ui_game_finished(pacman_won_the_game)
